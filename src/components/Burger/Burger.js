a94c1ae0632ca884dd88745173f86f2a69eeb3f3
wongprom <jimmybackstrom@hotmail.com>
2018-09-30 17:01:49 +0200

    Send object in state to Burger.js
    Convert prop Object to array to able map().

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,16 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
+  const transformedIngredients = Object.keys(props.ingredients).map(igkey => {
+    return [...Array(props.ingredients[igkey])].map((_, i) => {
+      return <BurgerIngredient key={igkey + i} type={igkey} />;
+    });
+  });
+  console.log(transformedIngredients);
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="salad" />
-      <BurgerIngredient type="meat" />
-      <BurgerIngredient type="bacon" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,18 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 2,
+      cheese: 1,
+      bacon: 1,
+      meat: 2
+    }
+  };
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Control</div>
       </Aux>
     );
