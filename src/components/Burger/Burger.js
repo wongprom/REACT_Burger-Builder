da162970f60016f8f1c4c12b38f493cf5c44c6f0
wongprom <jimmybackstrom@hotmail.com>
2018-10-03 21:08:32 +0200

    Flat array transformIngredients in BurgerBuilder.js to be able to use .length on array
    BEFORE reduce, array transformIngredients contains 4 empty inner arrays . After reduce(), transformIngredients only have one array that contains the values. Now I can use .length.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,17 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(igkey => {
-    return [...Array(props.ingredients[igkey])].map((_, i) => {
-      return <BurgerIngredient key={igkey + i} type={igkey} />;
-    });
-  });
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(igkey => {
+      return [...Array(props.ingredients[igkey])].map((_, i) => {
+        return <BurgerIngredient key={igkey + i} type={igkey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
   console.log(transformedIngredients);
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,10 +5,10 @@ import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
-      salad: 2,
-      cheese: 1,
-      bacon: 1,
-      meat: 2
+      salad: 0,
+      cheese: 0,
+      bacon: 0,
+      meat: 0
     }
   };
   render() {
