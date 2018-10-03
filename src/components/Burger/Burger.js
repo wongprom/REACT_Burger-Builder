d5800159f536032eb12ab1cdb8fc4e97e31efb30
wongprom <jimmybackstrom@hotmail.com>
2018-10-03 21:25:03 +0200

    If NO ingredients selected, output paragraph instead.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,7 +3,7 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(igkey => {
       return [...Array(props.ingredients[igkey])].map((_, i) => {
         return <BurgerIngredient key={igkey + i} type={igkey} />;
@@ -12,7 +12,10 @@ const burger = props => {
     .reduce((arr, el) => {
       return arr.concat(el);
     }, []);
-  console.log(transformedIngredients);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please add Ingredients to your burger </p>;
+  }
 
   return (
     <div className={classes.Burger}>
