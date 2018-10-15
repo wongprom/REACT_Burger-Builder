e237a2bcd11b5c6a4466b65c4a057474b205b2f1
wongprom <jimmybackstrom@hotmail.com>
2018-10-15 22:20:21 +0200

    Create UI controls that user can select what he/she wants on the burger

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './BuildControl.css';
 
 const buildControl = props => (
-  <div className={classes.BuiltControl}>
+  <div className={classes.BuildControl}>
     <div className={classes.Label}>{props.label}</div>
     <button className={classes.Less}>LESS</button>
     <button className={classes.More}>MORE</button>
