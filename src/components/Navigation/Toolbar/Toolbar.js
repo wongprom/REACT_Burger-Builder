e4c5798e8d1599c97bf850abc36cd8743972abae
wongprom <jimmybackstrom@hotmail.com>
2019-08-23 10:23:34 +0200

    Created li component and output to UI. Problem now is that active styling is not applied.

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from 'react'
 import classes from './Toolbar.css'
 import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
     <div>Menu</div>
     <Logo />
     <nav>
-      ...
-  </nav>
+      <NavigationItems />
+    </nav>
   </header>
 )
 
